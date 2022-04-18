f7987566dcfe84b6d9b3f7cacf9e82d8ef2bb974
philip06lee <philip06lee@gmail.com>
2022-04-18 20:53:54 +0800

    initial

--- next.config.js
+++ next.config.js
@@ -3,3 +3,15 @@ module.exports = {
     domains:["res.cloudinary.com"]
   }
 }
+
+// next.config.js
+module.exports = {
+  async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://gogo-best-philip06lee.vercel.app/api/login/:path*',
+        },
+      ]
+    },
+};
