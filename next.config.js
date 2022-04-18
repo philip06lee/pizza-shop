279297ca48413b7253eb4ec2e0ba1046a81df66b
philip06lee <philip06lee@gmail.com>
2022-04-18 21:25:53 +0800

    initial

--- next.config.js
+++ next.config.js
@@ -4,14 +4,3 @@ module.exports = {
   }
 }
 
-// next.config.js
-module.exports = {
-  async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'https://gogo-best-philip06lee.vercel.app/api/login/:path*',
-        },
-      ]
-    },
-};
